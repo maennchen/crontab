ff264c0043dd45361a9298b5810fad1c47af75f0
Jonatan Männchen <jonatan@maennchen.ch>
2016-12-22 13:51:38 +0000

    bump mix version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Crontab.Mixfile do
 
   def project do
     [app: :crontab,
-     version: "0.1.0",
+     version: "0.1.4",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
