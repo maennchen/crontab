2d81816571dafe1c1eae8acebc356a46b435906b
Jonatan Männchen <jonatan@maennchen.ch>
2016-12-23 22:12:22 +0000

    Document DateChecker properly

--- lib/crontab/cron_date_checker.ex
+++ lib/crontab/cron_date_checker.ex
@@ -8,16 +8,49 @@ defmodule Crontab.CronDateChecker do
 
   ### Examples
 
-      iex> Crontab.CronDateChecker.matches_date :hour, [{:"/", :*, 4}, 7], ~N[2004-04-16 04:07:08]
-      true
-
-      iex> Crontab.CronDateChecker.matches_date :hour, [8], ~N[2004-04-16 04:07:08]
-      false
-
       iex> Crontab.CronDateChecker.matches_date %Crontab.CronInterval{minute: [{:"/", :*, 8}]}, ~N[2004-04-16 04:08:08]
       true
 
       iex> Crontab.CronDateChecker.matches_date %Crontab.CronInterval{minute: [{:"/", :*, 9}]}, ~N[2004-04-16 04:07:08]
+      false
+
+  """
+  @spec matches_date(Crontab.ExtendedCronInterval.all_t, NaiveDateTime.t) :: boolean
+  def matches_date(cron_interval = %Crontab.CronInterval{}, execution_date) do
+    cron_interval
+      |> Crontab.CronInterval.to_condition_list
+      |> matches_date(execution_date)
+  end
+  def matches_date(cron_interval = %Crontab.ExtendedCronInterval{}, execution_date) do
+    cron_interval
+      |> Crontab.ExtendedCronInterval.to_condition_list
+      |> matches_date(execution_date)
+  end
+
+  @doc """
+  Check a condition list against a given date.
+
+  ### Examples
+
+      iex> Crontab.CronDateChecker.matches_date [{:hour, [{:"/", :*, 4}, 7]}], ~N[2004-04-16 04:07:08]
+      true
+  """
+  @spec matches_date(Crontab.ExtendedCronInterval.condition_list, NaiveDateTime.t) :: boolean
+  def matches_date([], _), do: true
+  def matches_date([{interval, conditions} | tail], execution_date) do
+    matches_date(interval, conditions, execution_date) && matches_date(tail, execution_date)
+  end
+
+
+  @doc """
+  Check a condition against a given date.
+
+  ### Examples
+
+      iex> Crontab.CronDateChecker.matches_date :hour, [{:"/", :*, 4}, 7], ~N[2004-04-16 04:07:08]
+      true
+
+      iex> Crontab.CronDateChecker.matches_date :hour, [8], ~N[2004-04-16 04:07:08]
       false
 
   """
@@ -31,22 +64,6 @@ defmodule Crontab.CronDateChecker do
     else
       matches_date(interval, tail, execution_date)
     end
-  end
-  @spec matches_date(Crontab.ExtendedCronInterval.all_t, NaiveDateTime.t) :: boolean
-  def matches_date(cron_interval = %Crontab.CronInterval{}, execution_date) do
-    cron_interval
-      |> Crontab.CronInterval.to_condition_list
-      |> matches_date(execution_date)
-  end
-  def matches_date(cron_interval = %Crontab.ExtendedCronInterval{}, execution_date) do
-    cron_interval
-      |> Crontab.ExtendedCronInterval.to_condition_list
-      |> matches_date(execution_date)
-  end
-  @spec matches_date(Crontab.ExtendedCronInterval.condition_list, NaiveDateTime.t) :: boolean
-  def matches_date([], _), do: true
-  def matches_date([{interval, conditions} | tail], execution_date) do
-    matches_date(interval, conditions, execution_date) && matches_date(tail, execution_date)
   end
 
   @spec matches_specific_date(Crontab.ExtendedCronInterval.interval, [integer], Crontab.CronInterval.value, NaiveDateTime.t) :: boolean
