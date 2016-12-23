e111eb8e731e82c1882a5e2a8251d96d830763d8
Jonatan Männchen <jonatan@maennchen.ch>
2016-12-23 16:11:29 +0000

    Improve Doc, clean coe

--- lib/crontab/cron_interval.ex
+++ lib/crontab/cron_interval.ex
@@ -6,14 +6,14 @@ defmodule Crontab.CronInterval do
   @doc """
   Defines the Cron Interval
 
-  \\* \\* \\* \\* \\* \\*\n
-  | | | | | |\n
-  | | | | | +-- :year Year                 (range: 1900-3000)\n
-  | | | | +---- :weekday Day of the Week   (range: 1-7, 1 standing for Monday)\n
-  | | | +------ :month Month of the Year   (range: 1-12)\n
-  | | +-------- :day Day of the Month      (range: 1-31)\n
-  | +---------- :hour Hour                 (range: 0-23)\n
-  +------------ :minute Minute             (range: 0-59)\n
+      * * * * * *
+      | | | | | |
+      | | | | | +-- :year Year                 (range: 1900-3000)
+      | | | | +---- :weekday Day of the Week   (range: 1-7, 1 standing for Monday)
+      | | | +------ :month Month of the Year   (range: 1-12)
+      | | +-------- :day Day of the Month      (range: 1-31)
+      | +---------- :hour Hour                 (range: 0-23)
+      +------------ :minute Minute             (range: 0-59)
   """
   defstruct minute: [:*], hour: [:*], day: [:*], month: [:*], weekday: [:*], year: [:*]
 
@@ -21,13 +21,13 @@ defmodule Crontab.CronInterval do
   Convert Crontab.CronInterval struct to Tuple List
 
   ### Examples
-    iex> Crontab.CronInterval.to_condition_list %Crontab.CronInterval{minute: [1], hour: [2], day: [3], month: [4], weekday: [5], year: [6]}
-    [ {:minute, [1]},
-      {:hour, [2]},
-      {:day, [3]},
-      {:month, [4]},
-      {:weekday, [5]},
-      {:year, [6]}]
+      iex> Crontab.CronInterval.to_condition_list %Crontab.CronInterval{minute: [1], hour: [2], day: [3], month: [4], weekday: [5], year: [6]}
+      [ {:minute, [1]},
+        {:hour, [2]},
+        {:day, [3]},
+        {:month, [4]},
+        {:weekday, [5]},
+        {:year, [6]}]
   """
   def to_condition_list(%Crontab.CronInterval{minute: minute, hour: hour, day: day, month: month, weekday: weekday, year: year}) do
     [ {:minute, minute},
