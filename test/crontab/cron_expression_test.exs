db238029de6ab0b32568a7dbd2a0b24986d47450
Jonatan Männchen <jonatan@maennchen.ch>
2017-01-20 00:50:45 +0000

    test Sigil Inspect

--- test/crontab/cron_expression_test.exs
+++ test/crontab/cron_expression_test.exs
@@ -1,7 +1,25 @@
 defmodule Crontab.CronExpressionTest do
+  import Crontab.CronExpression
+
   use ExUnit.Case, async: true
   doctest Crontab.CronExpression, import: true
 
+  import ExUnit.CaptureIO
+
   # I'd like to doctest that one, but I can't get the sigil working
   # doctest Inspect.Crontab.CronExpression, import: true
+
+  test "sigil inspect" do
+    fun = fn ->
+      assert IO.inspect ~e[*]
+    end
+    assert capture_io(fun) == "~e[* * * * * *]\n"
+  end
+
+  test "extended sigil inspect" do
+    fun = fn ->
+      assert IO.inspect ~e[*]e
+    end
+    assert capture_io(fun) == "~e[* * * * * * *]e\n"
+  end
 end
