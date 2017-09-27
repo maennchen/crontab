73106a5ff0abf0ae7566a855bc9841fc496f0d03
Jonatan Männchen <jonatan@maennchen.ch>
2017-09-27 08:43:31 +0000

    #45 Microsecond Precision Scheduler Fix

--- test/crontab/scheduler_test.exs
+++ test/crontab/scheduler_test.exs
@@ -27,6 +27,14 @@ defmodule Crontab.SchedulerTest do
     assert get_next_run_date(%Crontab.CronExpression{minute: [16]}, ~N[2002-01-13 23:00:07]) == {:ok, ~N[2002-01-13 23:16:00]}
   end
 
+  test "check cron expression for second" do
+    assert get_next_run_date(%Crontab.CronExpression{extended: true, second: [{:/, :*, 3}]}, ~N[2002-01-13 23:00:06]) == {:ok, ~N[2002-01-13 23:00:06]}
+  end
+
+  test "check cron expression given microseconds" do
+    assert get_next_run_date(%Crontab.CronExpression{extended: true, second: [{:/, :*, 3}]}, ~N[2002-01-13 23:00:06.00]) == {:ok, ~N[2002-01-13 23:00:06]}
+  end
+
   test "check combined" do
     assert get_next_run_date(%Crontab.CronExpression{minute: [3], hour: [7], day: [27], month: [2]}, ~N[2002-01-13 23:00:07]) == {:ok, ~N[2002-02-27 07:03:00]}
   end
