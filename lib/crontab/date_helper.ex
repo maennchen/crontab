9fb0fba82133d71ba3a22b8dacf730e6570b370c
Jonatan Männchen <jonatan@maennchen.ch>
2020-08-04 20:03:55 +0200

    DateHelper DateTime Support

--- lib/crontab/date_helper.ex
+++ lib/crontab/date_helper.ex
@@ -3,10 +3,12 @@ defmodule Crontab.DateHelper do
 
   @type unit :: :year | :month | :day | :hour | :minute | :second | :microsecond
 
+  @type date :: NaiveDateTime.t() | DateTime.t()
+
   @units [
     {:year, {nil, nil}},
     {:month, {1, 12}},
-    {:day, {1, :end_onf_month}},
+    {:day, {1, :end_of_month}},
     {:hour, {0, 23}},
     {:minute, {0, 59}},
     {:second, {0, 59}},
@@ -21,8 +23,11 @@ defmodule Crontab.DateHelper do
       iex> Crontab.DateHelper.beginning_of(~N[2016-03-14 01:45:45.123], :year)
       ~N[2016-01-01 00:00:00]
 
+      iex> Crontab.DateHelper.beginning_of(~U[2016-03-14 01:45:45.123Z], :year)
+      ~U[2016-01-01 00:00:00Z]
+
   """
-  @spec beginning_of(NaiveDateTime.t(), unit) :: NaiveDateTime.t()
+  @spec beginning_of(date, unit :: unit) :: date when date: date
   def beginning_of(date, unit) do
     _beginning_of(date, proceeding_units(unit))
   end
@@ -35,16 +40,28 @@ defmodule Crontab.DateHelper do
       iex> Crontab.DateHelper.end_of(~N[2016-03-14 01:45:45.123], :year)
       ~N[2016-12-31 23:59:59.999999]
 
+      iex> Crontab.DateHelper.end_of(~U[2016-03-14 01:45:45.123Z], :year)
+      ~U[2016-12-31 23:59:59.999999Z]
+
   """
-  @spec end_of(NaiveDateTime.t(), unit) :: NaiveDateTime.t()
+  @spec end_of(date, unit :: unit) :: date when date: date
   def end_of(date, unit) do
     _end_of(date, proceeding_units(unit))
   end
 
   @doc """
-  Find the last occurrence of weekday in month.
+  Find last occurrence of weekday in month
+
+  ### Examples:
+
+      iex> Crontab.DateHelper.last_weekday(~N[2016-03-14 01:45:45.123], 6)
+      26
+
+      iex> Crontab.DateHelper.last_weekday(~U[2016-03-14 01:45:45.123Z], 6)
+      26
+
   """
-  @spec last_weekday(NaiveDateTime.t(), Calendar.day_of_week()) :: Calendar.day()
+  @spec last_weekday(date :: date, day_of_week :: Calendar.day_of_week()) :: Calendar.day()
   def last_weekday(date, weekday) do
     date
     |> end_of(:month)
@@ -52,9 +69,19 @@ defmodule Crontab.DateHelper do
   end
 
   @doc """
-  Find the nth weekday of month.
+  Find nth weekday of month
+
+  ### Examples:
+
+      iex> Crontab.DateHelper.nth_weekday(~N[2016-03-14 01:45:45.123], 6, 2)
+      12
+
+      iex> Crontab.DateHelper.nth_weekday(~U[2016-03-14 01:45:45.123Z], 6, 2)
+      12
+
   """
-  @spec nth_weekday(NaiveDateTime.t(), Calendar.day_of_week(), integer) :: Calendar.day()
+  @spec nth_weekday(date :: date, weekday :: Calendar.day_of_week(), n :: pos_integer) ::
+          Calendar.day()
   def nth_weekday(date, weekday, n) do
     date
     |> beginning_of(:month)
@@ -62,7 +89,16 @@ defmodule Crontab.DateHelper do
   end
 
   @doc """
-  Find the last occurrence of weekday in month.
+  Find last occurrence of weekday in month
+
+  ### Examples:
+
+      iex> Crontab.DateHelper.last_weekday_of_month(~N[2016-03-14 01:45:45.123])
+      31
+
+      iex> Crontab.DateHelper.last_weekday_of_month(~U[2016-03-14 01:45:45.123Z])
+      31
+
   """
   @spec last_weekday_of_month(NaiveDateTime.t()) :: Calendar.day()
   def last_weekday_of_month(date) do
@@ -70,9 +106,18 @@ defmodule Crontab.DateHelper do
   end
 
   @doc """
-  Find the next occurrence of weekday relative to date.
+  Find next occurrence of weekday relative to date
+
+  ### Examples:
+
+      iex> Crontab.DateHelper.next_weekday_to(~N[2016-03-14 01:45:45.123])
+      14
+
+      iex> Crontab.DateHelper.next_weekday_to(~U[2016-03-14 01:45:45.123Z])
+      14
+
   """
-  @spec next_weekday_to(NaiveDateTime.t()) :: Calendar.day()
+  @spec next_weekday_to(date :: date) :: Calendar.day()
   def next_weekday_to(date = %NaiveDateTime{year: year, month: month, day: day}) do
     weekday = :calendar.day_of_the_week(year, month, day)
     next_day = NaiveDateTime.add(date, 1, :day)
@@ -87,8 +132,36 @@ defmodule Crontab.DateHelper do
     end
   end
 
-  @spec inc_year(NaiveDateTime.t()) :: NaiveDateTime.t()
-  def inc_year(date) do
+  def next_weekday_to(date = %DateTime{year: year, month: month, day: day}) do
+    weekday = :calendar.day_of_the_week(year, month, day)
+    # FIXME: How to correct date with tz?
+    next_day = DateTime.add(date, 1, :day)
+    # FIXME: How to correct date with tz?
+    previous_day = DateTime.add(date, -1, :day)
+
+    cond do
+      weekday == 7 && next_day.month == date.month -> next_day.day
+      weekday == 7 -> DateTime.add(date, -2, :day).day
+      weekday == 6 && previous_day.month == date.month -> previous_day.day
+      weekday == 6 -> DateTime.add(date, 2, :day).day
+      true -> date.day
+    end
+  end
+
+  @doc """
+  Increment Year
+
+  ### Examples:
+
+      iex> Crontab.DateHelper.inc_year(~N[2016-03-14 01:45:45.123])
+      ~N[2017-03-15 01:45:45.123]
+
+      iex> Crontab.DateHelper.inc_year(~U[2016-03-14 01:45:45.123Z])
+      ~U[2017-03-15 01:45:45.123Z]
+
+  """
+  @spec inc_year(date) :: date when date: date
+  def inc_year(date = %NaiveDateTime{}) do
     leap_year? =
       date
       |> NaiveDateTime.to_date()
@@ -101,8 +174,35 @@ defmodule Crontab.DateHelper do
     end
   end
 
-  @spec dec_year(NaiveDateTime.t()) :: NaiveDateTime.t()
-  def dec_year(date) do
+  def inc_year(date = %DateTime{}) do
+    leap_year? =
+      date
+      |> DateTime.to_date()
+      |> Date.leap_year?()
+
+    if leap_year? do
+      # FIXME: How to correct date with tz?
+      DateTime.add(date, 366, :day)
+    else
+      # FIXME: How to correct date with tz?
+      DateTime.add(date, 365, :day)
+    end
+  end
+
+  @doc """
+  Decrement Year
+
+  ### Examples:
+
+      iex> Crontab.DateHelper.dec_year(~N[2016-03-14 01:45:45.123])
+      ~N[2015-03-14 01:45:45.123]
+
+      iex> Crontab.DateHelper.dec_year(~U[2016-03-14 01:45:45.123Z])
+      ~U[2015-03-14 01:45:45.123Z]
+
+  """
+  @spec dec_year(date) :: date when date: date
+  def dec_year(date = %NaiveDateTime{}) do
     leap_year? =
       date
       |> NaiveDateTime.to_date()
@@ -115,7 +215,34 @@ defmodule Crontab.DateHelper do
     end
   end
 
-  @spec inc_month(NaiveDateTime.t()) :: NaiveDateTime.t()
+  def dec_year(date = %DateTime{}) do
+    leap_year? =
+      date
+      |> DateTime.to_date()
+      |> Date.leap_year?()
+
+    if leap_year? do
+      # FIXME: How to correct date with tz?
+      DateTime.add(date, -366, :day)
+    else
+      # FIXME: How to correct date with tz?
+      DateTime.add(date, -365, :day)
+    end
+  end
+
+  @doc """
+  Increment Month
+
+  ### Examples:
+
+      iex> Crontab.DateHelper.inc_month(~N[2016-03-14 01:45:45.123])
+      ~N[2016-04-01 01:45:45.123]
+
+      iex> Crontab.DateHelper.inc_month(~U[2016-03-14 01:45:45.123Z])
+      ~U[2016-04-01 01:45:45.123Z]
+
+  """
+  @spec inc_month(date) :: date when date: date
   def inc_month(date = %NaiveDateTime{day: day}) do
     days =
       date
@@ -125,30 +252,84 @@ defmodule Crontab.DateHelper do
     NaiveDateTime.add(date, days + 1 - day, :day)
   end
 
-  @spec dec_month(NaiveDateTime.t()) :: NaiveDateTime.t()
-  def dec_month(date) do
+  def inc_month(date = %DateTime{day: day}) do
     days =
       date
-      |> NaiveDateTime.to_date()
+      |> DateTime.to_date()
       |> Date.days_in_month()
 
-    NaiveDateTime.add(date, -days, :day)
+    # FIXME: How to correct date with tz?
+    DateTime.add(date, days + 1 - day, :day)
   end
 
-  @spec _beginning_of(NaiveDateTime.t(), [{unit, {any, any}}]) :: NaiveDateTime.t()
+  @doc """
+  Decrement Month
+
+  ### Examples:
+
+      iex> Crontab.DateHelper.dec_month(~N[2016-03-14 01:45:45.123])
+      ~N[2016-02-14 01:45:45.123]
+
+      iex> Crontab.DateHelper.dec_month(~U[2016-03-14 01:45:45.123Z])
+      ~U[2016-02-14 01:45:45.123Z]
+
+      iex> Crontab.DateHelper.dec_month(~N[2011-05-31 23:59:59])
+      ~N[2011-04-30 23:59:59]
+
+  """
+  @spec dec_month(date) :: date when date: date
+  def dec_month(date = %NaiveDateTime{day: day}) do
+    days_in_last_month =
+      date
+      |> NaiveDateTime.to_date()
+      |> day_in_last_month
+      |> Date.days_in_month()
+
+    NaiveDateTime.add(date, -(day + max(days_in_last_month - day, 0)), :day)
+  end
+
+  def dec_month(date = %DateTime{day: day}) do
+    days_in_last_month =
+      date
+      |> DateTime.to_date()
+      |> day_in_last_month
+      |> Date.days_in_month()
+
+    # FIXME: How to correct date with tz?
+    DateTime.add(date, -(day + max(days_in_last_month - day, 0)), :day)
+  end
+
+  defp day_in_last_month(start_date), do: day_in_last_month(start_date, start_date)
+
+  defp day_in_last_month(date = %Date{month: month}, start_date = %Date{month: month}),
+    do: date |> Date.add(-1) |> day_in_last_month(start_date)
+
+  defp day_in_last_month(date, _start_date), do: date
+
+  @spec _beginning_of(date, [{unit, {any, any}}]) :: date when date: date
   defp _beginning_of(date, [{unit, {lower, _}} | tail]) do
     _beginning_of(Map.put(date, unit, lower), tail)
   end
 
   defp _beginning_of(date, []), do: date
 
-  @spec _end_of(NaiveDateTime.t(), [{unit, {any, any}}]) :: NaiveDateTime.t()
-  defp _end_of(date, [{unit, {_, :end_onf_month}} | tail]) do
+  @spec _end_of(date, [{unit, {any, any}}]) :: date when date: date
+  defp _end_of(date = %NaiveDateTime{}, [{unit, {_, :end_of_month}} | tail]) do
     upper =
       date
       |> NaiveDateTime.to_date()
       |> Date.days_in_month()
 
+    _end_of(Map.put(date, unit, upper), tail)
+  end
+
+  defp _end_of(date = %DateTime{}, [{unit, {_, :end_of_month}} | tail]) do
+    upper =
+      date
+      |> DateTime.to_date()
+      |> Date.days_in_month()
+
+    # FIXME: How to correct date with tz?
     _end_of(Map.put(date, unit, upper), tail)
   end
 
@@ -165,7 +346,7 @@ defmodule Crontab.DateHelper do
       |> Enum.reduce([], fn {key, value}, acc ->
         cond do
           Enum.count(acc) > 0 ->
-            Enum.concat(acc, [{key, value}])
+            [{key, value} | acc]
 
           key == unit ->
             [{key, value}]
@@ -174,13 +355,19 @@ defmodule Crontab.DateHelper do
             []
         end
       end)
+      |> Enum.reverse()
 
     units
   end
 
-  @spec nth_weekday(NaiveDateTime.t(), Calendar.day_of_week(), :start) :: boolean
+  @spec nth_weekday(date :: date, weekday :: Calendar.day_of_week(), position :: :start) ::
+          boolean
   defp nth_weekday(date = %NaiveDateTime{}, _, 0, :start),
     do: NaiveDateTime.add(date, -1, :day).day
+
+  # FIXME: How to correct date with tz?
+  defp nth_weekday(date = %DateTime{}, _, 0, :start),
+    do: DateTime.add(date, -1, :day).day
 
   defp nth_weekday(date = %NaiveDateTime{year: year, month: month, day: day}, weekday, n, :start) do
     if :calendar.day_of_the_week(year, month, day) == weekday do
@@ -190,7 +377,17 @@ defmodule Crontab.DateHelper do
     end
   end
 
-  @spec last_weekday_of_month(NaiveDateTime.t(), :end) :: Calendar.day()
+  defp nth_weekday(date = %DateTime{year: year, month: month, day: day}, weekday, n, :start) do
+    if :calendar.day_of_the_week(year, month, day) == weekday do
+      # FIXME: How to correct date with tz?
+      nth_weekday(DateTime.add(date, 1, :day), weekday, n - 1, :start)
+    else
+      # FIXME: How to correct date with tz?
+      nth_weekday(DateTime.add(date, 1, :day), weekday, n, :start)
+    end
+  end
+
+  @spec last_weekday_of_month(date :: date(), position :: :end) :: Calendar.day()
   defp last_weekday_of_month(date = %NaiveDateTime{year: year, month: month, day: day}, :end) do
     weekday = :calendar.day_of_the_week(year, month, day)
 
@@ -201,12 +398,33 @@ defmodule Crontab.DateHelper do
     end
   end
 
-  @spec last_weekday(NaiveDateTime.t(), non_neg_integer, :end) :: Calendar.day()
+  defp last_weekday_of_month(date = %DateTime{year: year, month: month, day: day}, :end) do
+    weekday = :calendar.day_of_the_week(year, month, day)
+
+    if weekday > 5 do
+      # FIXME: How to correct date with tz?
+      last_weekday_of_month(DateTime.add(date, -1, :day), :end)
+    else
+      day
+    end
+  end
+
+  @spec last_weekday(date :: date, weekday :: Calendar.day_of_week(), position :: :end) ::
+          Calendar.day()
   defp last_weekday(date = %NaiveDateTime{year: year, month: month, day: day}, weekday, :end) do
     if :calendar.day_of_the_week(year, month, day) == weekday do
       day
     else
       last_weekday(NaiveDateTime.add(date, -1, :day), weekday, :end)
+    end
+  end
+
+  defp last_weekday(date = %DateTime{year: year, month: month, day: day}, weekday, :end) do
+    if :calendar.day_of_the_week(year, month, day) == weekday do
+      day
+    else
+      # FIXME: How to correct date with tz?
+      last_weekday(DateTime.add(date, -1, :day), weekday, :end)
     end
   end
 end
--- test/crontab/date_helper_test.exs
+++ test/crontab/date_helper_test.exs
@@ -6,7 +6,7 @@ defmodule Crontab.DateHelperTest do
   doctest Crontab.DateHelper
 
   describe "inc_month/1" do
-    test "does not jump obver month" do
+    test "does not jump over month" do
       assert Crontab.DateHelper.inc_month(~N[2019-05-31 23:00:00]) == ~N[2019-06-01 23:00:00]
     end
   end
