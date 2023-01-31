b7655bbaca5f28e27947c3410de3951df944d857
Thomas Timmer <thomas.timmer@bettyblocks.com>
2023-01-31 12:24:40 +0100

    feat: add skip year option to composer

--- test/crontab/cron_expression/composer_test.exs
+++ test/crontab/cron_expression/composer_test.exs
@@ -3,4 +3,19 @@ defmodule Crontab.CronExpression.ComposerTest do
   use ExUnit.Case, async: true
 
   doctest Crontab.CronExpression.Composer
+
+  alias Crontab.CronExpression.Composer
+  alias Crontab.CronExpression.Parser
+
+  test "works" do
+    specific_cron = "*/15 9-17 5-19 2-8 SUN 2012-2015"
+    cron_expression = Parser.parse!(specific_cron)
+    assert "*/15 9-17 5-19 2-8 7 2012-2015" == Composer.compose(cron_expression)
+  end
+
+  test "works with skip year flag" do
+    specific_cron = "*/15 9-17 5-19 2-8 SUN 2012-2015"
+    cron_expression = Parser.parse!(specific_cron)
+    assert "*/15 9-17 5-19 2-8 7" == Composer.compose(cron_expression, %{skip_year: true})
+  end
 end
